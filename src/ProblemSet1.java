539e4375509003ab95c6510aad9f79e748f15f78
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2019-09-16 09:07:05 -0400

    Exercise 1 works

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -1,53 +1,58 @@
 /**
  * Problem Set 1.
- * 
+ *
  * It's time to put your skills to the test. This problem set focuses on using
  * primitive data types, variables, and basic operators and functions.
- * 
+ *
  * The specifications for each exercise are outlined below. Your output is
  * expected to meet requirements set forth in this section (as well as the
  * Deliverables section).
- * 
+ *
  * Work through these exercises on your own. Experiment, make mistakes, ask
  * questions, and fix your mistakes. It's the only way to get good at programming.
  */
 
 public class ProblemSet1 {
-    
+
     public static void main(String[] args) {
-        
+
         /*
          * Exercise 1.
-         * 
+         *
          * What is the area (in square millimeters) of an 8.5-by-11-inch sheet of paper?
          */
-        double pageLength = 11;
-        
-        
+        final double pageLength = 11;
+        final double pageWidth = 8.5;
+        double pageAreaIn = pageWidth * pageLength;
+        final double squareIn2Mm = 645.16;
+        double pageAreaMm = pageAreaIn * squareIn2Mm;
+        System.out.printf("%,.2f square millimeters.", pageAreaMm);
+
+
         /*
          * Exercise 2.
-         * 
+         *
          * What is the perimeter (in centimeters) of an 8.5-by-11-inch sheet of paper?
          */
-        
-        
-        
+
+
+
         /*
          * Exercise 3.
-         * 
+         *
          * What is the length of the diagonal (in inches) between two corners on an 8.5-
          * by-11-inch sheet of paper?
          */
-        
-        
+
+
 
         /*
          * Exercise 4.
-         * 
+         *
          * Given the grading policy and the homework, quiz, and test grades I received,
          * what marking period grade will I get?
          */
-        
+
         int homework1 = 88;
         int homework2 = 91;
         int homework3 = 0;
@@ -57,58 +62,58 @@ public class ProblemSet1 {
         int test1 = 74;
         int test2 = 87;
         int test3 = 82;
-        
-        
-        
+
+
+
         /*
          * Exercise 5.
-         * 
+         *
          * I make $12.50/hour working as a cashier at a local supermarket. How much money
          * will I make this week?
          */
 
-        
-        
+
+
         /*
          * Exercise 6.
-         * 
+         *
          * What is my take-home pay each check?
          */
-        
-        
-        
+
+
+
         /*
          * Exercise 7.
-         * 
+         *
          * I am planning a class trip next month. How many buses do I need, and how many
          * people will be on the last bus?
          */
-        
-        
-        
+
+
+
         /*
          * Exercise 8.
-         * 
+         *
          * What is the surface area of a standard Cornhole board?
          */
-        
-        
-        
+
+
+
         /*
          * Exercise 9.
-         * 
+         *
          * Are the years 2020, 2100, and 2400 leap years?
          */
-        
-        
-        
+
+
+
         /*
          * Exercise 10.
-         * 
+         *
          * What is the wind chill?
          */
-        
-        
-        
+
+
+
     }
 }
